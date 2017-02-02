0558d618a782e7f15810a826eca3f92cd0581361
ylang365 <ylang365@gmail.com>
2017-02-03 01:35:42 +0900

    Update react-browser-with-addons.js

--- react-builds/react-browser-with-addons.js
+++ react-builds/react-browser-with-addons.js
@@ -1,8 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-
+var Mui = require("material-ui");
 
 React.addons = require("./addons-object.js");
 
 window.React = React;
 window.ReactDOM = ReactDOM;
+Window.Mui = Mui;
