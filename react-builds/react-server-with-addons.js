8cd393aec52f5b2e312686ff39c76159bd08fdb4
ylang365 <ylang365@gmail.com>
2017-02-03 01:37:08 +0900

    Update react-server-with-addons.js

--- react-builds/react-server-with-addons.js
+++ react-builds/react-server-with-addons.js
@@ -1,7 +1,9 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
+var Mui = require("material-ui");
 
 React.addons = require("./addons-object.js");
 
 global.React = React;
 global.ReactDOMServer = ReactDOMServer;
+global.Mui = Mui;
