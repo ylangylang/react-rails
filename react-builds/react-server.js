7b22113887df15d63be16cb5908d11e8e5a79d64
ylang365 <ylang365@gmail.com>
2017-02-03 01:37:45 +0900

    Update react-server.js

--- react-builds/react-server.js
+++ react-builds/react-server.js
@@ -1,5 +1,7 @@
 var React = require("react");
 var ReactDOMServer = require("react-dom/server");
+var Mui = require("material-ui");
 
 global.React = React;
 global.ReactDOMServer = ReactDOMServer;
+global.Mui = Mui;
